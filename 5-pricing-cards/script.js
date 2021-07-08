3f70ed25498e85a22f0e147c9500828ac57d10e6
dhananjayansb <dhananjayansb@gmail.com>
2021-07-08 11:37:40 +0530

    5-pricing-cards

--- 5-pricing-cards/script.js
+++ 5-pricing-cards/script.js
@@ -4,7 +4,7 @@ const master = document.getElementById('master');
 const basic = document.getElementById('basic');
 
 checkbox.addEventListener('click', () => {
-  basic.textContent = basic.textContent === '$199.99' ? '$19.99' : '$199.99';
-  professional.textContent = professional.textContent === '$249.99' ? '$24.99 ' : '$249.99';
-  master.textContent = master.textContent === '$399.99' ? '$39.99' : '$399.99';
+  basic.textContent = basic.textContent === '₹199.99' ? '₹19.99' : '₹199.99';
+  professional.textContent = professional.textContent === '₹249.99' ? '₹24.99 ' : '₹249.99';
+  master.textContent = master.textContent === '₹399.99' ? '₹39.99' : '₹399.99';
 });
